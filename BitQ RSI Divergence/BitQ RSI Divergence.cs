cb9def8a1cb847f27a49a030e4aed01cfacadd45
kyunkakata <kyunkakata@gmail.com>
2021-05-30 16:28:59 +0700

    feat(RSI Divergence): add bearish, bullish

--- BitQ RSI Divergence/BitQ RSI Divergence.cs
+++ BitQ RSI Divergence/BitQ RSI Divergence.cs
@@ -42,7 +42,7 @@ namespace cAlgo
             rsi = Indicators.RelativeStrengthIndex(Source, periods);
             IndicatorArea.DrawHorizontalLine("70", 70, Color.IndianRed, 1, LineStyle.LinesDots);
             IndicatorArea.DrawHorizontalLine("30", 30, Color.IndianRed, 1, LineStyle.LinesDots);
-            
+
         }
 
 
@@ -54,7 +54,7 @@ namespace cAlgo
             Result[index] = rsi.Result[index];
             //if (index >= 1029 && index <= 1037)
             //{
-                findPeakTrough(index);
+            findPeakTrough(index);
             //}
         }
 
@@ -76,6 +76,7 @@ namespace cAlgo
                         {
                             filterBearish();
                         }
+                        troughtList = new ArrayList();
                         isLastPeak = true;
                     }
                 }
@@ -84,9 +85,45 @@ namespace cAlgo
                     if (Result[index - 2] > Result[index - 1] && Result[index - 1] < Result[index])
                     {
                         IndicatorArea.DrawIcon("RSI_trought_" + (index - 1).ToString(), ChartIconType.Circle, index - 1, Result[index - 1], Color.Blue);
-                        IndicatorArea.DrawText("RSI_trought_t_" + (index - 1).ToString(), (index-1).ToString(), index - 1, Result[index - 1], Color.Blue);
+                        IndicatorArea.DrawText("RSI_trought_t_" + (index - 1).ToString(), (index - 1).ToString(), index - 1, Result[index - 1], Color.Blue);
+                        var point = new Point(index - 1, Result[index - 1], Bars.OpenTimes.Last(index - 1));
+                        troughtList.Add(point);
+                        if (!isLastPeak)
+                        {
+                            filterBullish();
+                        }
+                        
                         peakList = new ArrayList();
                         isLastPeak = false;
+                    }
+                }
+            }
+        }
+
+        public void filterBullish()
+        {
+            if (troughtList.Count >= 2)
+            {
+                Point currPoint = (Point)troughtList[troughtList.Count - 1];
+                for(int i= 0; i < troughtList.Count; i++)
+                {
+                    Point iPoint = (Point)troughtList[i];
+                    if (iPoint.value < currPoint.value)
+                    {
+                        if (!isBrokenLine(troughtList, i, troughtList.Count - 1, false))
+                        {
+                            if(isDivergenceTrought(iPoint, currPoint))
+                            {
+                                double open1 = Bars.OpenPrices[iPoint.index];
+                                double close1 = Bars.ClosePrices[iPoint.index];
+                                var trought1 = Math.Min(open1, close1);
+                                var open2 = Bars.OpenPrices[currPoint.index];
+                                var close2 = Bars.ClosePrices[currPoint.index];
+                                var trought2 = Math.Min(open2, close2);
+                                Chart.DrawTrendLine("Bullish_unconfirm1" + iPoint.index + "-" + currPoint.index, iPoint.index, trought1, currPoint.index, trought2, Color.Yellow, 2);
+                                IndicatorArea.DrawTrendLine("Bullish_unconfirm1" + iPoint.index + "-" + currPoint.index, iPoint.index, iPoint.value, currPoint.index, currPoint.value, Color.Yellow, 3);
+                            }
+                        }
                     }
                 }
             }
@@ -102,9 +139,9 @@ namespace cAlgo
                     Point iPoint = (Point)peakList[i];
                     if (iPoint.value > currPoint.value)
                     {
-                        if(!isBrokenLine(peakList, i, peakList.Count - 1))
+                        if (!isBrokenLine(peakList, i, peakList.Count - 1, true))
                         {
-                            if(isDivergencePeak(iPoint, currPoint))
+                            if (isDivergencePeak(iPoint, currPoint))
                             {
                                 double open1 = Bars.OpenPrices[iPoint.index];
                                 double close1 = Bars.ClosePrices[iPoint.index];
@@ -113,7 +150,7 @@ namespace cAlgo
                                 var close2 = Bars.ClosePrices[currPoint.index];
                                 var peak2 = Math.Max(open2, close2);
                                 Chart.DrawTrendLine("Bearish_unconfirm1" + iPoint.index + "-" + currPoint.index, iPoint.index, peak1, currPoint.index, peak2, Color.AliceBlue, 2);
-                                IndicatorArea.DrawTrendLine("Bearish_unconfirm1" + iPoint.index + "-" + currPoint.index, iPoint.index, iPoint.value, currPoint.index, currPoint.value, Color.Green, 3);
+                                IndicatorArea.DrawTrendLine("Bearish_unconfirm1" + iPoint.index + "-" + currPoint.index, iPoint.index, iPoint.value, currPoint.index, currPoint.value, Color.AliceBlue, 3);
                             }
                         }
                     }
@@ -131,27 +168,53 @@ namespace cAlgo
             var close2 = Bars.ClosePrices[endPoint.index];
             var peak2 = Math.Max(open2, close2);
 
-            if (peak1 < peak2) return true;
+            if (peak1 < peak2)
+                return true;
             return false;
         }
 
-        public bool isBrokenLine(ArrayList array, int startIndex, int endIndex)
+        public bool isDivergenceTrought(Point startPoint, Point endPoint)
+        {
+            double open1 = Bars.OpenPrices[startPoint.index];
+            double close1 = Bars.ClosePrices[startPoint.index];
+            var trough1 = Math.Min(open1, close1);
+            var open2 = Bars.OpenPrices[endPoint.index];
+            var close2 = Bars.ClosePrices[endPoint.index];
+            var trough2 = Math.Min(open2, close2);
+
+            if (trough1 > trough2)
+                return true;
+            return false;
+        }
+
+        public bool isBrokenLine(ArrayList array, int startIndex, int endIndex, bool isPeak)
         {
             bool isBrokenLine = false;
-            if (endIndex - startIndex == 1) return false;
+            if (endIndex - startIndex == 1)
+                return false;
             double b, w;
-            findLine((Point)peakList[startIndex], (Point)peakList[endIndex], out b, out w);
+            findLine((Point)array[startIndex], (Point)array[endIndex], out b, out w);
 
-            for(int i = startIndex + 1; i< endIndex; i++)
+            for (int i = startIndex + 1; i < endIndex; i++)
             {
                 Point iPoint = (Point)array[i];
                 var value = b * iPoint.index + w;
-                
-                if(value < iPoint.value)
+                if (isPeak)
                 {
-                    isBrokenLine = true;
-                    break;
+                    if (value < iPoint.value)
+                    {
+                        isBrokenLine = true;
+                        break;
+                    }
+                } else
+                {
+                    if (value > iPoint.value)
+                    {
+                        isBrokenLine = true;
+                        break;
+                    }
                 }
+                
             }
             return isBrokenLine;
         }
